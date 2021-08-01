86a4cc8cf727c2cb83b5fc2e646e8b236a45b8e9
marti <marti@Martin-Spectre13>
2021-08-01 10:12:24 +0100

    Updated atmosphere model temperature profile

--- init_Rocket_Sim_1.m
+++ init_Rocket_Sim_1.m
@@ -2,6 +2,7 @@
 g = 9.81; %Assuming g is roughly constant throughout flight
 p0 = 101325; %Sea level pressure in Pa
 gamma = 1.4; %Specific heat ratio for dry air
+R = 287; %Gas constnat for air
 sigma = 5.6704e-8; %Stefan-boltzmann constant in W/m^2.K
 
 m_dry = 50; %Dry mass of rocket in kg
@@ -17,7 +18,7 @@ m_dot = F/Ve; %Assume constant m_dot throughout flight (i.e. constant throttle s
 De = 50; %Nozzle exit diameter in mm
 Ae = pi*(De/2000)^2; %Nozzle exit area (should be calculated from engine design)
 
-r_n = 0.01; %Nose radius of nosecone tip in m (I think)
+r_n = 0.001; %Nose radius of nosecone tip in m (I think)
 epsilon = 0.1; %Emissivity coefficient of the nosecone tip
 
 Cd = 0.5; %Drag coefficient (neglecting supersonic drag for now)
@@ -40,13 +41,13 @@ for i = 1:numel(thrustValues)
     results = sim(mdl);
     
     subplot(3,1,1);
-    plot(results.logsout.get("Altitude (km)").Values)
+    plot(results.logsout.get("Velocity (ms^-^1)").Values)
     hold on
     subplot(3,1,2);
-    plot(results.logsout.get("Velocity (ms^-1)").Values)
+    plot(results.logsout.get("Convective heat transfer rate (W/cm^2)").Values)
     hold on
     subplot(3,1,3);
-    plot(results.logsout.get("Acceleration (ms^-2)").Values)
+    plot(results.logsout.get("Ambient Pressure (kPa)").Values)
     hold on
     
     %legendLabels{i} = "Thrust = " + num2str(F/1000) + "kN";
